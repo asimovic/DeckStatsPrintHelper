dbc0e648738ef393006f9f3aacc6cb19e5ad23c1
AlexS <none@none.com>
2018-05-15 21:37:21 -0400

    Fixed url resolution

--- DeckStatsPrintHelper.user.js
+++ DeckStatsPrintHelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Deck Stats Print Utility
-// @version      0.3
+// @version      0.4
 // @description  Fix printing issues on deck stats
 // @author       AlexS
 // @match        https://deckstats.net/decks/*proxies=*
@@ -17,33 +17,33 @@
 
     let printStyle = `
 <style type="text/css">
-  @media print {
-    hr {
-      display: none !important;
-    }
-    #cards_main {
-      overflow: hidden;
-      page-break-after: always;
-    }
-    .card_proxy {
-      float:left;
-    }
+@media print {
+  hr {
+    display: none !important;
   }
+  #cards_main {
+    overflow: hidden;
+    page-break-after: always;
+  }
+  .card_proxy {
+    float:left;
+  }
+}
 </style>`;
 
   $(printStyle).appendTo('head');
 
   $('#cards_main > img').each(function() {
-    console.log(this.currentSrc);
+
+    console.log(normalizeUrl($(this).attr('src')));
     GM_xmlhttpRequest({
         method: "GET",
-        url:  this.currentSrc,
+        url: normalizeUrl($(this).attr('src')),
         responseType: 'blob',
         onload: function (response) {
           processImage(response.response);
         },
         onerror: function (response) {
-            //alert('error');
             console.log(response);
         }
     });
@@ -56,10 +56,8 @@
     const imageUrl = URL.createObjectURL(imageData);
     $('#cards_main').prepend('<img id="testImg" />');
     $('#testImg').attr('src', imageUrl);
-
     let img = new Image();
     img.src = imageUrl;
-
     img.onload = function() {
       let canvas = document.createElement('canvas');
       if(canvas.getContext) {
@@ -73,15 +71,19 @@
           let data = ctx.getImageData(x, y, 1, 1).data;
           return [data[0], data[1], data[2], data[3]];
         };
-
         console.log(img.getPixel(20,20));
-
       } else {
         // canvas not supported, fall back
         img.getPixel = function(x,y) {return [0,0,0,0];};
       }
     };
+  }
 
+  function normalizeUrl(url) {
+    if (url.startsWith('//')) {
+      return location.protocol + url;
+    }
+    return url;
   }
 
 })();
