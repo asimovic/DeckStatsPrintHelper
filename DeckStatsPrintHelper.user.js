74e4f4fc0e7474b27f80de2588f1e72d65bbd170
AlexS <none@none.com>
2018-05-15 20:43:30 -0400

    Added prelim image processing

--- DeckStatsPrintHelper.user.js
+++ DeckStatsPrintHelper.user.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         Deck Stats Print Utility
-// @version      0.2
+// @version      0.3
 // @description  Fix printing issues on deck stats
 // @author       AlexS
 // @match        https://deckstats.net/decks/*proxies=*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @grant        none
+// @downloadURL  https://github.com/asimovic/DeckStatsPrintHelper/raw/master/DeckStatsPrintHelper.user.js
+// @updateURL    https://github.com/asimovic/DeckStatsPrintHelper/raw/master/DeckStatsPrintHelper.user.js
+// @connect      mtg-forum.de
+// @grant        GM_xmlhttpRequest
+// @license      MIT
 // ==/UserScript==
 
 (function() {
@@ -13,20 +17,71 @@
 
     let printStyle = `
 <style type="text/css">
-@media print {
-  hr {
-    display: none !important;
+  @media print {
+    hr {
+      display: none !important;
+    }
+    #cards_main {
+      overflow: hidden;
+      page-break-after: always;
+    }
+    .card_proxy {
+      float:left;
+    }
   }
-  #cards_main {
-    overflow: hidden;
-    page-break-after: always;
-  }
-  .card_proxy {
-    float:left;
-  }
-}
 </style>`;
 
   $(printStyle).appendTo('head');
-  
+
+  $('#cards_main > img').each(function() {
+    console.log(this.currentSrc);
+    GM_xmlhttpRequest({
+        method: "GET",
+        url:  this.currentSrc,
+        responseType: 'blob',
+        onload: function (response) {
+          processImage(response.response);
+        },
+        onerror: function (response) {
+            //alert('error');
+            console.log(response);
+        }
+    });
+
+    throw new Error('Stop');
+
+  });
+
+  function processImage(imageData) {
+    const imageUrl = URL.createObjectURL(imageData);
+    $('#cards_main').prepend('<img id="testImg" />');
+    $('#testImg').attr('src', imageUrl);
+
+    let img = new Image();
+    img.src = imageUrl;
+
+    img.onload = function() {
+      let canvas = document.createElement('canvas');
+      if(canvas.getContext) {
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        let ctx = canvas.getContext("2d");
+        ctx.drawImage(img,0,0);
+
+        img.getPixel = function(x, y) {
+          let data = ctx.getImageData(x, y, 1, 1).data;
+          return [data[0], data[1], data[2], data[3]];
+        };
+
+        console.log(img.getPixel(20,20));
+
+      } else {
+        // canvas not supported, fall back
+        img.getPixel = function(x,y) {return [0,0,0,0];};
+      }
+    };
+
+  }
+
 })();
