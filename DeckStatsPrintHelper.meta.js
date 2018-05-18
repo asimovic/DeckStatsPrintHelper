5a967915fd3bc05e773ddca4c1528b45aff44ff1
AlexS <none@none.com>
2018-05-17 20:54:23 -0400

    Added meta file Added border images

---
+++ DeckStatsPrintHelper.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Deck Stats Print Utility
+// @version      0.9
+// @description  Fix printing issues on deck stats
+// @author       AlexS
+// @match        https://deckstats.net/deck*proxies=*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @downloadURL  https://github.com/asimovic/DeckStatsPrintHelper/raw/master/DeckStatsPrintHelper.user.js
+// @updateURL    https://github.com/asimovic/DeckStatsPrintHelper/raw/master/DeckStatsPrintHelper.meta.js
+// @connect      mtg-forum.de
+// @grant        GM_xmlhttpRequest
+// @license      MIT
+// ==/UserScript==
