b254deb8240941fdad1a765450018159d66fd7d7
AlexS <none@none.com>
2018-05-19 16:50:16 -0400

    Added lazy border image loading Added dynamic border color support Code re-organization

--- DeckStatsPrintHelper.meta.js
+++ DeckStatsPrintHelper.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Deck Stats Print Utility
-// @version      1.1
+// @version      1.2
 // @description  Fix printing issues on deck stats
 // @author       AlexS
 // @match        https://deckstats.net/deck*proxies=*
